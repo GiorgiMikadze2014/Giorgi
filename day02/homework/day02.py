84bae4395fadaada8327b11590d1e79f24c09627
GiorgiMikadze2014 <giorgigeorgia2014@gmail.com>
2025-04-26 11:19:19 +0100

    commit

---
+++ day02/homework/day02.py
@@ -0,0 +1,33 @@
+number1 = 20
+number2 = 10
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+
+number1 = 50
+number2 = 25
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+
+
+number1 = 40
+number2 = 20
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+
+
+number1 = 60
+number2 = 30
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
